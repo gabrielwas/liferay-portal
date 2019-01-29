50aa4147eb22d142af054c1ab7e08309f5b83366
Gabriel Albuquerque <gwas@cin.ufpe.br>
2019-01-29 17:56:29 -0300

    LPS-89134 Add Data Record Collection delete tests

--- modules/apps/data-engine/data-engine-test/src/testIntegration/java/com/liferay/data/engine/service/test/DEDataEngineTestUtil.java
+++ modules/apps/data-engine/data-engine-test/src/testIntegration/java/com/liferay/data/engine/service/test/DEDataEngineTestUtil.java
@@ -23,6 +23,7 @@ import com.liferay.data.engine.service.DEDataDefinitionSaveResponse;
 import com.liferay.data.engine.service.DEDataDefinitionService;
 import com.liferay.data.engine.service.DEDataRecordCollectionDeleteModelPermissionsRequest;
 import com.liferay.data.engine.service.DEDataRecordCollectionDeletePermissionsRequest;
+import com.liferay.data.engine.service.DEDataRecordCollectionDeleteRequest;
 import com.liferay.data.engine.service.DEDataRecordCollectionRequestBuilder;
 import com.liferay.data.engine.service.DEDataRecordCollectionSaveRequest;
 import com.liferay.data.engine.service.DEDataRecordCollectionSaveResponse;
@@ -44,6 +45,35 @@ import java.util.Map;
  * @author Leonardo Barros
  */
 public class DEDataEngineTestUtil {
+
+	public static void deleteDEDataRecordCollection(
+			User user, long groupId, long deDataRecordCollectionId,
+			DEDataRecordCollectionService deDataRecordCollectionService)
+		throws Exception {
+
+		PermissionThreadLocal.setPermissionChecker(
+			PermissionCheckerFactoryUtil.create(user));
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(groupId, user.getUserId());
+
+		ServiceContextThreadLocal.pushServiceContext(serviceContext);
+
+		try {
+			DEDataRecordCollectionDeleteRequest
+				deDataRecordCollectionDeleteRequest =
+					DEDataRecordCollectionRequestBuilder.deleteBuilder(
+					).byId(
+						deDataRecordCollectionId
+					).build();
+
+			deDataRecordCollectionService.execute(
+				deDataRecordCollectionDeleteRequest);
+		}
+		finally {
+			ServiceContextThreadLocal.popServiceContext();
+		}
+	}
 
 	public static void deleteDEDataRecordCollectionModelPermissions(
 			long companyId, User user, long groupId,
