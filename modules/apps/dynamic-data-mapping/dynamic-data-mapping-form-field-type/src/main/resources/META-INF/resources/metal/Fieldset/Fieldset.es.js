7cec75150b7a198db6b0c216014d7faa250a801a
Bruno Basto <bruno.basto@liferay.com>
2019-04-17 16:01:55 -0300

    LPS-93027 Implements Fieldset

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/metal/Fieldset/Fieldset.es.js
@@ -0,0 +1,150 @@
+import '../FieldBase/FieldBase.es';
+import './FieldsetRegister.soy.js';
+import Component from 'metal-component';
+import Soy from 'metal-soy';
+import templates from './Fieldset.soy.js';
+import {Config} from 'metal-state';
+
+class Fieldset extends Component {
+	static STATE = {
+
+		/**
+		 * @default undefined
+		 * @instance
+		 * @memberof Fieldset
+		 * @type {?(number|undefined)}
+		 */
+
+		columnSize: Config.number(),
+
+		/**
+		 * @default undefined
+		 * @instance
+		 * @memberof Fieldset
+		 * @type {?(string|undefined)}
+		 */
+
+		fieldName: Config.string(),
+
+		/**
+		 * @default undefined
+		 * @instance
+		 * @memberof Fieldset
+		 * @type {?(string|undefined)}
+		 */
+
+		label: Config.string(),
+
+		/**
+		 * @default undefined
+		 * @instance
+		 * @memberof Fieldset
+		 * @type {?(string|undefined)}
+		 */
+
+		name: Config.string().required(),
+
+		/**
+		 * @default undefined
+		 * @instance
+		 * @memberof Fieldset
+		 * @type {?(array|undefined)}
+		 */
+
+		nestedFields: Config.array(),
+
+		/**
+		 * @default undefined
+		 * @instance
+		 * @memberof Fieldset
+		 * @type {?(string|undefined)}
+		 */
+
+		placeholder: Config.string(),
+
+		/**
+		 * @default false
+		 * @instance
+		 * @memberof Fieldset
+		 * @type {?bool}
+		 */
+
+		readOnly: Config.bool().value(false),
+
+		/**
+		 * @default undefined
+		 * @instance
+		 * @memberof Fieldset
+		 * @type {?(bool|undefined)}
+		 */
+
+		repeatable: Config.bool(),
+
+		/**
+		 * @default false
+		 * @instance
+		 * @memberof Fieldset
+		 * @type {?(bool|undefined)}
+		 */
+
+		required: Config.bool().value(false),
+
+		/**
+		 * @default true
+		 * @instance
+		 * @memberof Fieldset
+		 * @type {?(bool|undefined)}
+		 */
+
+		showLabel: Config.bool().value(true),
+
+		/**
+		 * @default undefined
+		 * @instance
+		 * @memberof Fieldset
+		 * @type {?(string|undefined)}
+		 */
+
+		spritemap: Config.string(),
+
+		/**
+		 * @default undefined
+		 * @instance
+		 * @memberof Fieldset
+		 * @type {?(string|undefined)}
+		 */
+
+		tip: Config.string(),
+
+		/**
+		 * @default undefined
+		 * @instance
+		 * @memberof Fieldset
+		 * @type {?(string|undefined)}
+		 */
+
+		tooltip: Config.string(),
+
+		/**
+		 * @default undefined
+		 * @instance
+		 * @memberof Fieldset
+		 * @type {?(string|undefined)}
+		 */
+
+		type: Config.string().value('fieldset'),
+
+		/**
+		 * @default undefined
+		 * @instance
+		 * @memberof Fieldset
+		 * @type {?(string|undefined)}
+		 */
+
+		value: Config.string().value('')
+	};
+}
+
+Soy.register(Fieldset, templates);
+
+export default Fieldset;
