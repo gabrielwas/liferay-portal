d924cdae4f6cbb693d6daa52863ba17b24c83f84
Gabriel Albuquerque <gwas@cin.ufpe.br>
2019-07-18 17:40:21 -0300

    LPS-97943 Create upgrade process

---
+++ modules/apps/app-builder/app-builder-service/src/main/java/com/liferay/app/builder/internal/upgrade/AppBuilderServiceUpgrade.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.app.builder.internal.upgrade;
+
+import com.liferay.app.builder.internal.upgrade.v1_0_0.UpgradeSchema;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Gabriel Albuquerque
+ */
+@Component(
+	immediate = true,
+	service = {AppBuilderServiceUpgrade.class, UpgradeStepRegistrator.class}
+)
+public class AppBuilderServiceUpgrade implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.register("1.0.0", "1.1.0", new UpgradeSchema());
+	}
+
+}
---
+++ modules/apps/app-builder/app-builder-service/src/main/resources/com/liferay/app/builder/internal/upgrade/v1_0_0/dependencies/update.sql
@@ -0,0 +1,17 @@
+create table AppBuilderApp (
+	uuid_ VARCHAR(75) null,
+	appBuilderAppId LONG not null primary key,
+	groupId LONG,
+	companyId LONG,
+	userId LONG,
+	userName VARCHAR(75) null,
+	createDate DATE null,
+	modifiedDate DATE null,
+	deDataLayoutId LONG,
+	deDataListViewId LONG,
+	settings_ VARCHAR(75) null
+);
+
+create index IX_B60BFFDD on AppBuilderApp (groupId);
+create index IX_EC1E021 on AppBuilderApp (uuid_[$COLUMN_LENGTH:75$], companyId);
+create unique index IX_65D5FAE3 on AppBuilderApp (uuid_[$COLUMN_LENGTH:75$], groupId);
