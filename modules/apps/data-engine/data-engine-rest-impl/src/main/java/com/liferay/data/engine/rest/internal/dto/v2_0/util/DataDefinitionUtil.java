e35c143d2a04d70a8f07fde9dd4405bd10e831d2
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2020-02-13 13:42:33 -0300

    Serialize and deserialize DataRules

--- modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/dto/v2_0/util/DataDefinitionUtil.java
+++ modules/apps/data-engine/data-engine-rest-impl/src/main/java/com/liferay/data/engine/rest/internal/dto/v2_0/util/DataDefinitionUtil.java
@@ -17,6 +17,7 @@ package com.liferay.data.engine.rest.internal.dto.v2_0.util;
 import com.liferay.data.engine.field.type.util.LocalizedValueUtil;
 import com.liferay.data.engine.rest.dto.v2_0.DataDefinition;
 import com.liferay.data.engine.rest.dto.v2_0.DataDefinitionField;
+import com.liferay.dynamic.data.mapping.form.builder.converter.DDMFormRuleConverter;
 import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldType;
 import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldTypeServicesTracker;
 import com.liferay.dynamic.data.mapping.form.field.type.DDMFormFieldTypeSettings;
@@ -55,6 +56,7 @@ public class DataDefinitionUtil {
 
 	public static DataDefinition toDataDefinition(
 			DDMFormFieldTypeServicesTracker ddmFormFieldTypeServicesTracker,
+			DDMFormRuleConverter ddmFormRuleConverter,
 			DDMStructure ddmStructure)
 		throws Exception {
 
@@ -71,6 +73,7 @@ public class DataDefinitionUtil {
 				dateCreated = ddmStructure.getCreateDate();
 				dateModified = ddmStructure.getModifiedDate();
 				defaultDataLayout = DataLayoutUtil.toDataLayout(
+					ddmFormRuleConverter,
 					ddmStructure.fetchDDMStructureLayout());
 				defaultLanguageId = LanguageUtil.getLanguageId(
 					ddmForm.getDefaultLocale());
