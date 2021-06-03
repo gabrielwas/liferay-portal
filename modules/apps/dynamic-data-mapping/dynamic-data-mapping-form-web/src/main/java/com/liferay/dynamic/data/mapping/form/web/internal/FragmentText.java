099811a81898cbde5cb94a2cbd4544b5b3ceaf5b
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2021-06-03 10:18:47 -0300

    WIP - Add text field fragment

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/FragmentText.java
@@ -0,0 +1,197 @@
+package com.liferay.dynamic.data.mapping.form.web.internal;
+
+import com.liferay.dynamic.data.mapping.form.renderer.DDMFormRenderer;
+import com.liferay.dynamic.data.mapping.form.renderer.DDMFormRenderingContext;
+import com.liferay.dynamic.data.mapping.model.DDMForm;
+import com.liferay.dynamic.data.mapping.model.DDMFormInstance;
+import com.liferay.dynamic.data.mapping.model.DDMFormInstanceSettings;
+import com.liferay.dynamic.data.mapping.model.DDMFormInstanceVersion;
+import com.liferay.dynamic.data.mapping.model.DDMStructureVersion;
+import com.liferay.dynamic.data.mapping.service.DDMFormInstanceLocalService;
+import com.liferay.dynamic.data.mapping.service.DDMFormInstanceVersionLocalService;
+import com.liferay.fragment.renderer.FragmentRenderer;
+import com.liferay.fragment.renderer.FragmentRendererContext;
+import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.resource.bundle.ResourceBundleLoader;
+import com.liferay.portal.kernel.resource.bundle.ResourceBundleLoaderUtil;
+import com.liferay.portal.kernel.util.AggregateResourceBundle;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+@Component(service = FragmentRenderer.class)
+public class FragmentText implements FragmentRenderer {
+
+	@Override
+	public String getCollectionKey() {
+		return "text-field";
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		return "text-field";
+	}
+
+	@Override
+	public void render(
+		FragmentRendererContext fragmentRendererContext,
+		HttpServletRequest httpServletRequest,
+		HttpServletResponse httpServletResponse) throws IOException {
+
+		try {
+
+			DDMFormInstance ddmFormInstance = _getDDMFormInstance();
+
+			DDMForm ddmForm = ddmFormInstance.getDDMForm();
+
+			DDMFormInstanceVersion latestDDMFormInstanceVersion = _ddmFormInstanceVersionLocalService.getLatestFormInstanceVersion(
+				ddmFormInstance.getFormInstanceId(), WorkflowConstants.STATUS_APPROVED);
+
+			DDMStructureVersion ddmStructureVersion =
+				latestDDMFormInstanceVersion.getStructureVersion();
+
+			httpServletRequest.setAttribute(
+				"fragmentRendererContext", fragmentRendererContext);
+
+			String ddmFormHTML = _ddmFormRenderer.render(
+				ddmForm, ddmStructureVersion.getDDMFormLayout(),
+				createDDMFormRenderingContext(ddmForm, httpServletRequest,
+					httpServletResponse));
+
+			setNamespacedAttribute(httpServletRequest, "content", ddmFormHTML);
+
+			_jspRenderer.renderJSP(_servletContext,
+				httpServletRequest, httpServletResponse, "/frag/page.jsp");
+
+		}
+		catch (PortalException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void setNamespacedAttribute(
+		HttpServletRequest httpServletRequest, String key, Object value) {
+
+		if (value instanceof Boolean) {
+			value = String.valueOf(value);
+		}
+		else if (value instanceof Number) {
+			value = String.valueOf(value);
+		}
+
+		httpServletRequest.setAttribute(key, value);
+	}
+
+	protected String getRedirectURL() {
+		DDMFormInstance ddmFormInstance = _getDDMFormInstance();
+
+		if (ddmFormInstance == null) {
+			return StringPool.BLANK;
+		}
+
+		try {
+			DDMFormInstanceSettings ddmFormInstanceSettings =
+				ddmFormInstance.getSettingsModel();
+
+			return ddmFormInstanceSettings.redirectURL();
+		}
+		catch (PortalException ignored) {
+
+		}
+
+		return StringPool.BLANK;
+	}
+
+	protected DDMFormRenderingContext createDDMFormRenderingContext(
+		DDMForm ddmForm, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+
+		DDMFormRenderingContext ddmFormRenderingContext =
+			new DDMFormRenderingContext();
+
+		DDMFormInstance ddmFormInstance = _getDDMFormInstance();
+
+		ddmFormRenderingContext.setContainerId(
+			"form_" + StringUtil.randomString());
+
+		ddmFormRenderingContext.setGroupId(ddmFormInstance.getGroupId());
+
+		ddmFormRenderingContext.setHttpServletRequest(httpServletRequest);
+
+		ddmFormRenderingContext.setHttpServletResponse(httpServletResponse);
+
+		ddmFormRenderingContext.setLocale(getLocale(httpServletRequest, ddmForm));
+		ddmFormRenderingContext.setViewMode(true);
+
+		ddmFormRenderingContext.setReadOnly(true);
+
+		//setDDMFormValues(ddmFormRenderingContext, ddmForm);
+
+		ddmFormRenderingContext.setPortletNamespace(
+			_portal.getPortletNamespace(
+				_portal.getPortletId(httpServletRequest)));
+		ddmFormRenderingContext.setShowSubmitButton(false);
+
+		return ddmFormRenderingContext;
+	}
+
+	@Reference
+	private Portal _portal;
+
+	private DDMFormInstance _getDDMFormInstance(){
+		return _ddmFormInstanceLocalService.fetchFormInstance(39444);
+	}
+
+	protected Locale getLocale(
+		HttpServletRequest httpServletRequest, DDMForm ddmForm) {
+
+		Locale locale = LocaleUtil.fromLanguageId(
+			LanguageUtil.getLanguageId(httpServletRequest));
+
+		if (ddmForm == null) {
+			return locale;
+		}
+
+		Set<Locale> availableLocales = ddmForm.getAvailableLocales();
+
+		if (availableLocales.contains(locale)) {
+			return locale;
+		}
+
+		return ddmForm.getDefaultLocale();
+	}
+
+	@Reference
+	private DDMFormInstanceLocalService _ddmFormInstanceLocalService;
+
+	@Reference
+	private DDMFormRenderer _ddmFormRenderer;
+
+	@Reference
+	private DDMFormInstanceVersionLocalService
+		_ddmFormInstanceVersionLocalService;
+
+	@Reference
+	private JSPRenderer _jspRenderer;
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.dynamic.data.mapping.form.web)",
+		unbind = "-"
+	)
+	private ServletContext _servletContext;
+
+}
