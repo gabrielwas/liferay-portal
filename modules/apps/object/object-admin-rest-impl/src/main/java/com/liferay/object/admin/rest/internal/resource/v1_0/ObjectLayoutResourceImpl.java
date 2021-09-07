594f23ddec05fe4d4ac32f4617022ddee7065799
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2021-09-07 18:54:47 -0300

    LPS-138860 Implement Layout resources

--- modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/resource/v1_0/ObjectLayoutResourceImpl.java
+++ modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/resource/v1_0/ObjectLayoutResourceImpl.java
@@ -14,9 +14,15 @@
 
 package com.liferay.object.admin.rest.internal.resource.v1_0;
 
+import com.liferay.object.admin.rest.dto.v1_0.ObjectLayout;
 import com.liferay.object.admin.rest.resource.v1_0.ObjectLayoutResource;
+import com.liferay.object.service.ObjectLayoutLocalService;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
+import com.liferay.portal.vulcan.util.LocalizedMapUtil;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -27,4 +33,57 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = ObjectLayoutResource.class
 )
 public class ObjectLayoutResourceImpl extends BaseObjectLayoutResourceImpl {
+
+	@Override
+	public Page<ObjectLayout> getObjectDefinitionObjectLayoutsPage(
+		Long objectDefinitionId, Pagination pagination) {
+
+		_objectLayoutLocalService.getObjectLayouts(
+			objectDefinitionId, pagination.getStartPosition(),
+			pagination.getEndPosition());
+
+		return Page.of(
+			transform(
+				_objectLayoutLocalService.getObjectLayouts(
+					objectDefinitionId, pagination.getStartPosition(),
+					pagination.getEndPosition()),
+				this::_toObjectLayout),
+			pagination,
+			_objectLayoutLocalService.getObjectLayoutsCount(
+				objectDefinitionId));
+	}
+
+	@Override
+	public ObjectLayout postObjectDefinitionObjectLayout(
+			Long objectDefinitionId, ObjectLayout objectLayout)
+		throws Exception {
+
+		return _toObjectLayout(
+			_objectLayoutLocalService.addObjectLayout(
+				contextUser.getUserId(), objectDefinitionId,
+				objectLayout.getDefaultObjectLayout(),
+				LocalizedMapUtil.getLocalizedMap(objectLayout.getName())));
+	}
+
+	private ObjectLayout _toObjectLayout(
+		com.liferay.object.model.ObjectLayout serviceBuilderObjectLayout) {
+
+		return new ObjectLayout() {
+			{
+				dateCreated = serviceBuilderObjectLayout.getCreateDate();
+				dateModified = serviceBuilderObjectLayout.getModifiedDate();
+				defaultObjectLayout =
+					serviceBuilderObjectLayout.getDefaultObjectLayout();
+				id = serviceBuilderObjectLayout.getObjectLayoutId();
+				name = LocalizedMapUtil.getI18nMap(
+					serviceBuilderObjectLayout.getNameMap());
+				objectDefinitionId =
+					serviceBuilderObjectLayout.getObjectDefinitionId();
+			}
+		};
+	}
+
+	@Reference
+	private ObjectLayoutLocalService _objectLayoutLocalService;
+
 }
