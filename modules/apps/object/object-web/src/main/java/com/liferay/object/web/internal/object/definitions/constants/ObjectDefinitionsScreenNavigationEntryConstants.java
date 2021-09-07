0a118ad143db720aea356689d11641a8a747d536
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2021-09-07 18:14:07 -0300

    LPS-138860 Add new constants

--- modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/definitions/constants/ObjectDefinitionsScreenNavigationEntryConstants.java
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/definitions/constants/ObjectDefinitionsScreenNavigationEntryConstants.java
@@ -22,6 +22,8 @@ public class ObjectDefinitionsScreenNavigationEntryConstants {
 
 	public static final String CATEGORY_KEY_DETAILS = "details";
 
+	public static final String CATEGORY_KEY_LAYOUTS = "layouts";
+
 	public static final String CATEGORY_KEY_FIELDS = "fields";
 
 	public static final String CATEGORY_KEY_OBJECTS = "objects";
@@ -29,6 +31,8 @@ public class ObjectDefinitionsScreenNavigationEntryConstants {
 	public static final String CATEGORY_KEY_PICKLISTS = "picklists";
 
 	public static final String ENTRY_KEY_DETAILS = "details";
+
+	public static final String ENTRY_KEY_LAYOUTS = "layouts";
 
 	public static final String ENTRY_KEY_FIELDS = "fields";
 
