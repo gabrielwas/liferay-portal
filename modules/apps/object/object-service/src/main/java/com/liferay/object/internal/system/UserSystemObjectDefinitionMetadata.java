548923f7d2bda6d6ebe1628d9b0eab237559074b
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2023-04-04 14:54:29 -0300

    LPS-180738 Add new system field lastLoginDate for User

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/system/UserSystemObjectDefinitionMetadata.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/system/UserSystemObjectDefinitionMetadata.java
@@ -124,6 +124,9 @@ public class UserSystemObjectDefinitionMetadata
 				"Text", "firstName", "String", "first-name", "givenName", true,
 				true),
 			createObjectField(
+				"Date", "lastLoginDate", "Date", "last-login-date",
+				"lastLoginDate", false, true),
+			createObjectField(
 				"Text", "uuid_", "String", "uuid", "uuid", false, true));
 	}
 
