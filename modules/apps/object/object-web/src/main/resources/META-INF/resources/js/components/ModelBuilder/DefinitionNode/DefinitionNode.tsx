38a18d40b1d22c0721a72cb2dc68ff12e7457f47
Rebeca Silva <rebeca.silva@liferay.com>
2023-09-01 20:49:03 -0300

    LPS-193646 Change the type declaration of setShowModal in some files

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/DefinitionNode/DefinitionNode.tsx
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModelBuilder/DefinitionNode/DefinitionNode.tsx
@@ -253,9 +253,12 @@ export function DefinitionNode({
 			{showModal.deleteObjectDefinition && (
 				<ModalDeleteObjectDefinition
 					handleOnClose={() => {
-						setShowModal({
-							deleteObjectDefinition: false,
-						});
+						setShowModal(
+							(previousState: Partial<ModelBuilderModals>) => ({
+								...previousState,
+								deleteObjectDefinition: false,
+							})
+						);
 					}}
 					objectDefinition={
 						deletedObjectDefinition as DeletedObjectDefinition
