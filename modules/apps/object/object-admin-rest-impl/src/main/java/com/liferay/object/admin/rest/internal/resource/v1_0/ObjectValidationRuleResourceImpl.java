ec13765c1228033329ba53d8d444cf633bb4bed3
Pedro Leite <pedro.leite@liferay.com>
2023-09-14 11:14:59 -0300

    LPS-196070 Add ERC to DTO and resource methods calls

--- modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/resource/v1_0/ObjectValidationRuleResourceImpl.java
+++ modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/resource/v1_0/ObjectValidationRuleResourceImpl.java
@@ -194,7 +194,8 @@ public class ObjectValidationRuleResourceImpl
 
 		return _toObjectValidationRule(
 			_objectValidationRuleService.addObjectValidationRule(
-				null, objectDefinitionId,
+				objectValidationRule.getExternalReferenceCode(),
+				objectDefinitionId,
 				GetterUtil.getBoolean(objectValidationRule.getActive()),
 				objectValidationRule.getEngine(),
 				LocalizedMapUtil.getLocalizedMap(
@@ -233,7 +234,8 @@ public class ObjectValidationRuleResourceImpl
 
 		return _toObjectValidationRule(
 			_objectValidationRuleService.updateObjectValidationRule(
-				null, objectValidationRuleId, objectValidationRule.getActive(),
+				objectValidationRule.getExternalReferenceCode(),
+				objectValidationRuleId, objectValidationRule.getActive(),
 				objectValidationRule.getEngine(),
 				LocalizedMapUtil.getLocalizedMap(
 					objectValidationRule.getErrorLabel()),
