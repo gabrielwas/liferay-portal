6ebcc2a7f038920a4f93e95a21e49b68ae4714ce
Pedro Leite <pedro.leite@liferay.com>
2023-09-14 14:05:56 -0300

    LPS-196070 Add addOrUpdateObjectValidationRule method

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectValidationRuleLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectValidationRuleLocalServiceImpl.java
@@ -135,6 +135,51 @@ public class ObjectValidationRuleLocalServiceImpl
 		return objectValidationRule;
 	}
 
+	@Indexable(type = IndexableType.REINDEX)
+	@Override
+	public ObjectValidationRule addOrUpdateObjectValidationRule(
+			String externalReferenceCode, long objectValidationRuleId,
+			long userId, long objectDefinitionId, boolean active, String engine,
+			Map<Locale, String> errorLabelMap, Map<Locale, String> nameMap,
+			String outputType, String script, boolean system,
+			List<ObjectValidationRuleSetting> objectValidationRuleSettings)
+		throws PortalException {
+
+		ObjectValidationRule existingObjectValidationRule = null;
+
+		if (objectValidationRuleId > 0) {
+			existingObjectValidationRule =
+				objectValidationRulePersistence.fetchByPrimaryKey(
+					objectValidationRuleId);
+		}
+
+		if ((existingObjectValidationRule == null) &&
+			Validator.isNotNull(externalReferenceCode)) {
+
+			ObjectDefinition objectDefinition =
+				_objectDefinitionPersistence.findByPrimaryKey(
+					objectDefinitionId);
+
+			existingObjectValidationRule =
+				objectValidationRulePersistence.fetchByERC_C_ODI(
+					externalReferenceCode, objectDefinition.getCompanyId(),
+					objectDefinitionId);
+		}
+
+		if (existingObjectValidationRule != null) {
+			return updateObjectValidationRule(
+				externalReferenceCode,
+				existingObjectValidationRule.getObjectValidationRuleId(),
+				active, engine, errorLabelMap, nameMap, outputType, script,
+				objectValidationRuleSettings);
+		}
+
+		return addObjectValidationRule(
+			externalReferenceCode, userId, objectDefinitionId, active, engine,
+			errorLabelMap, nameMap, outputType, script, system,
+			objectValidationRuleSettings);
+	}
+
 	@Indexable(type = IndexableType.DELETE)
 	@Override
 	public ObjectValidationRule deleteObjectValidationRule(
