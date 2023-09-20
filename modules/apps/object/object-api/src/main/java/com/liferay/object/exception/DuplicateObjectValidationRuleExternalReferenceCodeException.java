a9778c85836a6c1aa1a1f1ec98c17e8ac4636edd
Pedro Leite <pedro.leite@liferay.com>
2023-09-20 11:03:30 -0300

    LPS-196070 SF

--- modules/apps/object/object-api/src/main/java/com/liferay/object/exception/DuplicateObjectValidationRuleExternalReferenceCodeException.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/exception/DuplicateObjectValidationRuleExternalReferenceCodeException.java
@@ -2,6 +2,7 @@
  * SPDX-FileCopyrightText: (c) 2023 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
+
 package com.liferay.object.exception;
 
 import com.liferay.portal.kernel.exception.DuplicateExternalReferenceCodeException;
@@ -9,20 +10,27 @@ import com.liferay.portal.kernel.exception.DuplicateExternalReferenceCodeExcepti
 /**
  * @author Marco Leo
  */
-public class DuplicateObjectValidationRuleExternalReferenceCodeException extends DuplicateExternalReferenceCodeException {
+public class DuplicateObjectValidationRuleExternalReferenceCodeException
+	extends DuplicateExternalReferenceCodeException {
 
 	public DuplicateObjectValidationRuleExternalReferenceCodeException() {
 	}
 
-	public DuplicateObjectValidationRuleExternalReferenceCodeException(String msg) {
+	public DuplicateObjectValidationRuleExternalReferenceCodeException(
+		String msg) {
+
 		super(msg);
 	}
 
-	public DuplicateObjectValidationRuleExternalReferenceCodeException(String msg, Throwable throwable) {
+	public DuplicateObjectValidationRuleExternalReferenceCodeException(
+		String msg, Throwable throwable) {
+
 		super(msg, throwable);
 	}
 
-	public DuplicateObjectValidationRuleExternalReferenceCodeException(Throwable throwable) {
+	public DuplicateObjectValidationRuleExternalReferenceCodeException(
+		Throwable throwable) {
+
 		super(throwable);
 	}
 
