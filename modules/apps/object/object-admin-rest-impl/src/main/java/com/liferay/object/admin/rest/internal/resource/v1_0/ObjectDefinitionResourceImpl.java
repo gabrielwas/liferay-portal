e83fca40f676a6cdb02828bc874ea76ab2f144c7
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2023-09-20 17:55:52 -0300

    LPS-000 WIP

--- modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/resource/v1_0/ObjectDefinitionResourceImpl.java
+++ modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/resource/v1_0/ObjectDefinitionResourceImpl.java
@@ -641,6 +641,29 @@ public class ObjectDefinitionResourceImpl
 			_getAccountEntryRestrictedObjectRelationshipsNames(
 				serviceBuilderObjectDefinition, objectRelationships);
 
+		Set<String> deleteObjectRelationshipsNames =
+			SetUtil.asymmetricDifference(
+				transform(
+					_objectRelationshipLocalService.getObjectRelationships(
+						objectDefinitionId),
+					com.liferay.object.model.ObjectRelationship::getName),
+				transformToList(
+					objectRelationships, ObjectRelationship::getName));
+
+		for (String deleteObjectRelationshipsName :
+			deleteObjectRelationshipsNames) {
+
+			com.liferay.object.model.ObjectRelationship
+				serviceBuilderObjectRelationship =
+				_objectRelationshipLocalService.
+					fetchObjectRelationshipByObjectDefinitionId(
+						objectDefinitionId,
+						deleteObjectRelationshipsName);
+
+			_objectRelationshipLocalService.deleteObjectRelationship(
+				serviceBuilderObjectRelationship.getObjectRelationshipId());
+		}
+
 		ObjectValidationRule[] objectValidationRules =
 			objectDefinition.getObjectValidationRules();
 
@@ -739,29 +762,6 @@ public class ObjectDefinitionResourceImpl
 		}
 
 		if (objectRelationships != null) {
-			Set<String> deleteObjectRelationshipsNames =
-				SetUtil.asymmetricDifference(
-					transform(
-						_objectRelationshipLocalService.getObjectRelationships(
-							objectDefinitionId),
-						com.liferay.object.model.ObjectRelationship::getName),
-					transformToList(
-						objectRelationships, ObjectRelationship::getName));
-
-			for (String deleteObjectRelationshipsName :
-					deleteObjectRelationshipsNames) {
-
-				com.liferay.object.model.ObjectRelationship
-					serviceBuilderObjectRelationship =
-						_objectRelationshipLocalService.
-							fetchObjectRelationshipByObjectDefinitionId(
-								objectDefinitionId,
-								deleteObjectRelationshipsName);
-
-				_objectRelationshipLocalService.deleteObjectRelationship(
-					serviceBuilderObjectRelationship.getObjectRelationshipId());
-			}
-
 			ObjectRelationshipResource.Builder builder =
 				_objectRelationshipResourceFactory.create();
 
