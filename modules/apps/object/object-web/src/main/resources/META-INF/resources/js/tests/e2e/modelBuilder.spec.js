08c8153d47bc681e6a8ca5cbdcda15d7c80ded5d
Gabriel Albuquerque <gabriel.albuquerque@liferay.com>
2023-11-28 17:40:11 -0300

    LPS-202448 Remove tests

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/tests/e2e/modelBuilder.spec.js
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/tests/e2e/modelBuilder.spec.js
@@ -22,67 +22,6 @@ export default function createTests() {
 		).toBeVisible();
 	});
 
-	test('can create custom objects', async ({page}) => {
-		await page.goto('/');
-
-		await page.getByLabel('Open Applications MenuCtrl+').click();
-		await page.getByRole('tab', {name: 'Control Panel'}).click();
-		await page.getByRole('link', {name: 'Objects'}).click();
-		await page.locator('li').filter({hasText: 'New Folder'}).click();
-
-		const objectNamePrefix = 'New Test Object';
-
-		for (let i = 1; i < 3; i++) {
-			await page.getByLabel('Create New Object').click();
-			await page.getByLabel('Label', {exact: true}).click();
-			await page
-				.getByLabel('Label', {exact: true})
-				.fill(`${objectNamePrefix} ${i}`);
-			await page.getByLabel('Label', {exact: true}).press('Tab');
-			await page
-				.getByLabel('Plural Label')
-				.fill(`${objectNamePrefix}s ${i}`);
-			await page.getByRole('button', {name: 'Save'}).click();
-			await expect(
-				page.getByRole('link', {name: `${objectNamePrefix} ${i}`})
-			).toBeVisible();
-		}
-	});
-
-	test('can edit external reference code of custom objects', async ({
-		page,
-	}) => {
-		await page.goto('/');
-
-		await page.getByLabel('Open Applications MenuCtrl+').click();
-		await page.getByRole('tab', {name: 'Control Panel'}).click();
-		await page.getByRole('link', {name: 'Objects'}).click();
-
-		const objectNamePrefix = 'New Test Object';
-
-		for (let i = 1; i < 3; i++) {
-			await page.locator('li').filter({hasText: 'New Folder'}).click();
-
-			await page.getByText(`${objectNamePrefix} ${i}`).click();
-			await page.getByLabel('Edit External Reference Code').click();
-
-			// await page.getByLabel('External Reference Code', {exact: true}).click();
-
-			await page
-				.getByLabel('External Reference Code', {exact: true})
-				.fill(`new-test-object-${i}`);
-			await page
-				.getByRole('dialog')
-				.getByRole('button', {name: 'Save'})
-				.click();
-			await expect(
-				page.getByText('Your request completed successfully.')
-			).toBeVisible();
-
-			await page.getByRole('link', {name: 'Back'}).click();
-		}
-	});
-
 	test('uncategorized folder does not contains delete and edit options', async ({
 		page,
 	}) => {
