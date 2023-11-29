266da8948231b29aa1975615bda3cd6b3d4b193f
Carlos Montenegro <carlos.montenegro@liferay.com>
2023-11-29 20:30:50 -0300

    LPS-202448 Test only in chromium for now

--- modules/test/playwright/playwright.config.js
+++ modules/test/playwright/playwright.config.js
@@ -12,14 +12,6 @@ module.exports = defineConfig({
 			name: 'chromium',
 			use: {...devices['Desktop Chrome']},
 		},
-		{
-			name: 'firefox',
-			use: {...devices['Desktop Firefox']},
-		},
-		{
-			name: 'webkit',
-			use: {...devices['Desktop Safari']},
-		},
 	],
 	reporter: [
 		[
